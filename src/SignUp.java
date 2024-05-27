e3a82904859cb7c0244c944402ca95a9128a1191
SK <snehakokh@gmail.com>
2024-05-27 22:19:14 +0545

    Finished

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
-
+import java.time.LocalDate;
 public class SignUp {
-    public static void SignUpProcesses(){
+    public static void SignUpProcesses() {
         Scanner scanner = new Scanner(System.in);
-        while (true){
+        while (true) {
             System.out.println("Please enter your full name:");
             String fullName = scanner.nextLine();
             if (!isFullNameCorrect(fullName)) {
@@ -13,46 +12,67 @@ public class SignUp {
             }
             System.out.println("Please enter your mobile number (username):");
             String phoneNo = scanner.nextLine();
-            if (!isPhoneNoCorrect(phoneNo)){
+            if (!isPhoneNoCorrect(phoneNo)) {
                 System.out.println("The mobile number must have 10 digits starting with 0");
                 continue;
             }
             System.out.println("Please enter your password:");
             String password = scanner.nextLine();
-            if (!isPwCorrect(password)){
+            if (!isPwCorrect(password)) {
                 System.out.println("The Password must initiate with alphabets followed by either one of @, & and ending with numeric.");
-            }else {
+            } else {
                 break;
             }
 
-        //    System.out.println("Please enter your password:");
-        //    String password1 = scanner.nextLine();
-        //    if (!isPasswordCorrect(password1)) {
-        //        System.out.println("Password must start with alphabets, followed by one of @, & and ending with numeric.");
-         //       continue;
-         //   }
+            System.out.println("Please confirm your password:");
+            String password1 = scanner.nextLine();
+            if (password1.equals(password)) {
+                continue;
+            } else {
+                System.out.println("Password must start with alphabets, followed by one of @, & and ending with numeric.");
+            }
 
-         //  System.out.println("Please enter your Date of Birth #DD/MM/YYYY (No space):");
-        //    String dob = scanner.nextLine();
-         //   if (!isDobCorrect(dob)){
-         //       System.out.println("You have entered the Date of Birth in invalid format.Please start again.");
-          //      continue;
-         //   }
-        //    System.out.println("Checking your age:.....");
-         //   if (!ageChecker(dob)){
-          //      System.out.println("You must be at least 21 years old. >:( ");
+            System.out.println("Please enter your Date of Birth #DD/MM/YYYY (No space):");
+            String dob = scanner.nextLine();
+            if (!isDobCorrect(dob)) {
+                System.out.println("You have entered the Date of Birth in invalid format.Please start again.");
+            }
+            System.out.println("Checking your age:.....");
+            if (!ageChecker(dob)) {
+                System.out.println("You must be at least 21 years old. >:( ");
             }
         }
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+
     private static boolean isFullNameCorrect(String fullName) {
         return fullName.length() > 4;
     }
 
-    private static boolean isPhoneNoCorrect(String phoneNo){
+    private static boolean isPhoneNoCorrect(String phoneNo) {
 
         return phoneNo.matches("0\\d{9}");
     }
 
-    private static boolean isPwCorrect(String password){
+    private static boolean isPwCorrect(String password) {
         return password.matches("^[A-Za-z]+[@&]\\\\d+$");
     }
-}
+
+
+    private static boolean isDobCorrect(String dob) {
+        String regex = "^(\\d{2}/\\d{2}/\\d{4})$";
+        return dob.matches(regex);
+    }
+
+    private static boolean ageChecker(String dob) {
+            String[] parts = dob.split("/");
+            int year = Integer.parseInt(parts[2]);
+            int currentYear = LocalDate.now().getYear();
+            return (currentYear - year) >= 21;
+        }
+    }
+
+
+
